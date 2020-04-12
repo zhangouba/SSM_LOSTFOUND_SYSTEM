9544c91d0192f0666119b27c9d845ec997c1be02
1139861012@qq.com <1139861012@qq.com>
2020-04-12 21:20:52 +0800

    输出三

--- src/main/java/com/qst/common/pojo/Test.java
+++ src/main/java/com/qst/common/pojo/Test.java
@@ -3,6 +3,7 @@ package com.qst.common.pojo;
 public class Test {
     public static void main(String[] args) {
         System.out.println(1);
- System.out.println(2);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
